686f20750aaca9855d48c23eaa148782de466e60
Avdeenko Ekaterina <avdeenqoeka@gmail.com>
2023-12-17 19:57:08 +0100

    local-maxima-remove_AvdeenkoEkaterina

--- src/main/java/com/epam/rd/autotasks/arrays/LocalMaximaRemove.java
+++ src/main/java/com/epam/rd/autotasks/arrays/LocalMaximaRemove.java
@@ -4,58 +4,58 @@ import java.util.Arrays;
 
 public class LocalMaximaRemove {
 
-    public static void main(String[] args) {
-        int[] array = new int[]{18, 1, 3, 6, 7, -5};
+  public static void main(String[] args) {
+    int[] array = new int[]{18, 1, 3, 6, 7, -5};
 
-        System.out.println(Arrays.toString(removeLocalMaxima(array)));
-    }
+    System.out.println(Arrays.toString(removeLocalMaxima(array)));
+  }
 
-    public static int[] removeLocalMaxima(int[] array) {
+  public static int[] removeLocalMaxima(int[] array) {
 
-        int localMaxNumber = 0; //количество локальных максимумов
-        for (int i=0; i < array.length; i++) {
-            if (i == 0) {
-                if (array[i] > array[i+1]) {
-                    localMaxNumber++;
-                    System.out.println(array[i]);
-                }
-            } else if (i == array.length-1) {
-                if (array[i] > array[i-1]) {
-                    localMaxNumber++;
-                    System.out.println(array[i]);
-                }
-            } else {
-                if (array[i] > array[i-1] && array[i] > array[i+1]) {
-                    localMaxNumber++;
-                    System.out.println(array[i]);
-                }
-            }
+    int localMaxNumber = 0;
+    for (int i = 0; i < array.length; i++) {
+      if (i == 0) {
+        if (array[i] > array[i + 1]) {
+          localMaxNumber++;
+          System.out.println(array[i]);
         }
-        int[] array2 = new int[array.length - localMaxNumber];
-        if (localMaxNumber != 0) {
-            int notLocalMaxNumberArrayIndex = 0; //индекс нелокального максимума в массиве array2
-            for (int i = 0; i < array.length; i++) {
-                if (i == 0) {
-                    if (array[i] < array[i + 1]) {
-                        System.arraycopy(array, i, array2, i, 1);
-                        notLocalMaxNumberArrayIndex++;
-                    }
-                } else if (i == array.length-1) {
-                    if (array[i] < array[i - 1]) {
-                        System.arraycopy(array, i, array2, array2.length - 1, 1 );
-                    }
-                } else {
-                    if (array[i] <= array[i-1] || array[i] <= array[i+1]) {
-                        System.arraycopy(array, i, array2, notLocalMaxNumberArrayIndex, 1);
-                        notLocalMaxNumberArrayIndex++;
-                    }
-                }
-            }
+      } else if (i == array.length - 1) {
+        if (array[i] > array[i - 1]) {
+          localMaxNumber++;
+          System.out.println(array[i]);
+        }
+      } else {
+        if (array[i] > array[i - 1] && array[i] > array[i + 1]) {
+          localMaxNumber++;
+          System.out.println(array[i]);
+        }
+      }
+    }
+    int[] array2 = new int[array.length - localMaxNumber];
+    if (localMaxNumber != 0) {
+      int notLocalMaxNumberArrayIndex = 0;
+      for (int i = 0; i < array.length; i++) {
+        if (i == 0) {
+          if (array[i] < array[i + 1]) {
+            System.arraycopy(array, i, array2, i, 1);
+            notLocalMaxNumberArrayIndex++;
+          }
+        } else if (i == array.length - 1) {
+          if (array[i] < array[i - 1]) {
+            System.arraycopy(array, i, array2, array2.length - 1, 1);
+          }
         } else {
-            return array;
+          if (array[i] <= array[i - 1] || array[i] <= array[i + 1]) {
+            System.arraycopy(array, i, array2, notLocalMaxNumberArrayIndex, 1);
+            notLocalMaxNumberArrayIndex++;
+          }
         }
-        return array2;
+      }
+    } else {
+      return array;
     }
+    return array2;
+  }
 }
 
 
